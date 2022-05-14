6803d8c688ec2b826fef39f1472d9eff12572c01
faith62 <phifeichemu@gmail.com>
2022-05-15 01:02:59 +0300

    define that relationship inside our Roles model

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class Role(db.Model): #define all the different roles
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
 
     def __repr__(self):
         return f'User {self.name}'
