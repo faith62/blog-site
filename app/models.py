9b72a771ef314d797db981d4dbcee26341c2f5b4
faith62 <phifeichemu@gmail.com>
2022-05-15 18:44:39 +0300

    create a template file for our profile

--- app/models.py
+++ app/models.py
@@ -14,10 +14,11 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(255))
+ 
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
 
     
     @property # decorator to create a write only class
