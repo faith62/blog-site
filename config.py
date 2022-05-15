e8fdc5815013f46f8b120e99da7dce46f484d3ee
faith62 <phifeichemu@gmail.com>
2022-05-15 17:33:04 +0300

    export secret key at strat.sh file

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://faith:1234@localhost/watchlist'
 
 
