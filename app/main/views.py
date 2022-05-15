eda32379a7dd8550446b1b202f918b674c73f786
faith62 <phifeichemu@gmail.com>
2022-05-15 18:41:37 +0300

    create profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template,redirect,request,url_for
+from flask import render_template,redirect,request,url_for,abort
 from . import main
 from flask_login import login_required
+from ..models import User
 
 # Views
 @main.route('/')
@@ -12,3 +13,12 @@ def index():
     '''
     title = 'one minute pitch'
     return render_template('index.html',title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
