439614b9f24b21d2fe4cae4a909e4d1b05211888
faith62 <phifeichemu@gmail.com>
2022-05-17 11:15:51 +0300

    update navbar

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,7 +11,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email = form.email.data).first()
-        if user is not None and User.verify_password(form.password.data):
+        if user is not None and user.verify_password(form.password.data):
             login_user(user,form.remember.data)
             return redirect(request.arg.get('next') or url_for('main.index'))
 
